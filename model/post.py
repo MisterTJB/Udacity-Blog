e2c7f4797d4561e676986fd66a40a3eba3da1b8c
Tim <tim.bathgate@gmail.com>
2016-11-22 14:57:44 +1300

    Update Post model
    - A Post can now have content
    - Submitters are now recorded using their username as an identifier

--- model/post.py
+++ model/post.py
@@ -3,5 +3,6 @@ from google.appengine.ext import ndb as db
 class Post(db.Model):
   title = db.StringProperty()
   votes = db.IntegerProperty()
+  content = db.TextProperty()
   submitted = db.DateTimeProperty(auto_now=True)
-  submitter = db.IntegerProperty()
+  submitter = db.StringProperty()
