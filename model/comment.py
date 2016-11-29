83a4c84e35a991fd760888057b4a0740b8df6959
Tim <tim.bathgate@gmail.com>
2016-11-30 09:57:38 +1300

    Add comments and improve style

--- model/comment.py
+++ model/comment.py
@@ -1,7 +1,19 @@
 from google.appengine.ext import ndb as db
 
+
 class Comment(db.Model):
-  content = db.TextProperty()
-  submitted = db.DateTimeProperty(auto_now_add=True)
-  submitter = db.StringProperty()
-  post_id = db.IntegerProperty()
+    """
+    Models a Comment on a blog.
+
+    Attributes:
+        content: The copy for a blog post
+        submitted: The submission datetime for a blog post. Set automatically
+            on insert, but not on update
+        submitter: The username of the user that posted the comment
+        post_id: The identifier for the post that this comment is associated
+            with
+    """
+    content = db.TextProperty()
+    submitted = db.DateTimeProperty(auto_now_add=True)
+    submitter = db.StringProperty()
+    post_id = db.IntegerProperty()
